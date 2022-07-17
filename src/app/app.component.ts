5618d0d6fb6e47cc95fa17b497702080def03c71
Gawesh1998 <gawesh2020java@gmail.com>
2022-07-17 20:12:14 +0530

    angular-sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cosmos-ems';
+  title = 'COSMOS Employee Management Solution';
+  fullName: string = ''
 }
