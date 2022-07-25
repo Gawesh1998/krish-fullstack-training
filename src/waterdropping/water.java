f492553953c08dc4308a213ad2d224c2f67c0335
Gawesh1998 <gawesh2020java@gmail.com>
2022-07-25 15:26:34 +0530

    waterdropping1

---
+++ src/waterdropping/water.java
@@ -0,0 +1,16 @@
+package waterdropping;
+
+public class water {
+
+    int h;
+    int w;
+    float water;
+
+    water(int h, int w, float water)
+    {
+        this.h = h;
+        this.w = w;
+        this.water = water;
+    }
+
+}
